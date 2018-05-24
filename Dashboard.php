fb803c4c7650515b16552209f7127219ac87b89f
ivanderwilliam <ivanderwilliam@gmail.com>
2018-05-24 17:43:46 +0700

    Create Dashboard.php

---
+++ Dashboard.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="Logo Website.png">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.css">
+    <link rel="stylesheet" href="/css/master.css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.js">
+    
+        
+<link rel="stylesheet" href="style/style.css">
+      
+    <title>Welcome</title>
+  </head>
+  <body>
+     <div class="ui inverted large menu">
+    <a class="active item">Home</a>
+    <a class="item">Order</a>
+    <a href="#" class="item">Gallery</a>
+    <a href="#" class="item">About</a>
+<div class="right menu">
+<div class="ui buttons">
+  <button class="ui button login">
+    <a class="ui white" href="login.php" style="color:#03A9F4;">
+      Log In</a>
+  </button>
+    <div class="or"></div>
+    <button class="ui positive button">
+      <a class="ui blue" href="signup.php" style="color:white;">Sign Up</a>
+    </button>
+</div>
+</div>
+</div>
+      
+      
+      
+  <div class="ui grid">
+    <div class="four wide column">
+      <div class="ui secondary vertical pointing fluid menu">
+        <a class="item">  
+            <div class="left floated column">
+                <i class="home icon"></i>
+                    Home
+            </div>            
+        </a>
+        <a class="item">  
+            <div class="left floated column">
+                <i class="utensils icon"></i>
+                    Order
+            </div>            
+        </a>
+        <a class="item">  
+            <div class="left floated column">
+                <i class="user circle icon"></i>
+                    Profile
+            </div>            
+        </a>
+        <a class="item">  
+            <div class="left floated column">
+                <i class="info circle icon"></i>
+                    About
+            </div>            
+        </a>
+        <a class="item">  
+            <div class="left floated column">
+                <i class="shopping cart icon"></i>
+                    Cart
+            </div>            
+        </a>
+      </div>
+    </div>
+    <div class="twelve wide column">
+        <div>
+        <div class="eight wide column">
+            Order By    :
+<div style="width:200px">
+<select name="skills" single="" class="ui fluid dropdown">
+  <option value="Murah">Termurah</option>
+<option value="Mahal">Termahal</option>
+<option value="Laris">Terlaris</option>
+        
+</select>
+            </div>
+        </div>
+        <div class="four wide column"><div class="ui fluid category search">
+            <div class="ui icon input">
+                <input class="prompt" type="text" placeholder="Search animals...">
+                    <i class="search icon"></i>
+            </div>
+            <div class="results"></div>
+            </div>
+        </div>
+    </div>
+  </div>
+ </div>
+    </body>
+ 
+</html>
