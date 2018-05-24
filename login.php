4ee3abffc77ffa9f4e2b7478d4197ac672f0b710
ivanderwilliam <ivanderwilliam@gmail.com>
2018-05-24 15:03:59 +0700

    Update login.php

--- login.php
+++ login.php
@@ -12,11 +12,39 @@
 
   </style>
   <body>
-    <br>
-    <br>
-    <br>
-    <br>
-    <h1 class="ui centered header">Log In</h1>
-
+   <div class="page-login">
+  <div class="ui centered grid container">
+    <div class="nine wide column">
+      <br><br>
+        <div class="ui icon warning message">
+          <i class="lock icon"></i>
+          <div class="content">
+            <div class="header">
+             <br> Login failed!
+            </div>
+            <p>You might have misspelled your username or password!</p>
+          </div>
+        </div>
+      <div class="ui fluid card">
+        <div class="content">
+        <form class="ui form" method="POST">
+          <div class="field">
+            <label>User</label>
+            <input type="text" name="user" placeholder="User">
+          </div>
+          <div class="field">
+            <label>Password</label>
+            <input type="password" name="pass" placeholder="Password">
+          </div>
+          <button class="ui primary labeled icon button" type="submit">
+            <i class="unlock alternate icon"></i>
+            Login
+          </button>
+        </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
   </body>
 </html>
