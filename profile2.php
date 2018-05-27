c03cf85debf115267e46f5743df417ec44df2fea
ivanderwilliam <ivanderwilliam@gmail.com>
2018-05-27 19:11:38 +0700

    Update profile2.php

--- profile2.php
+++ profile2.php
@@ -5,8 +5,8 @@
 <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<script src="js/Profile.js"></script>
-<link href="style/style.css">
+<script src="js/Profile2.js"></script>
+<link href="style/style2.css">
 <!------ Include the above in your HEAD tag ---------->
 </head>
 <body>
