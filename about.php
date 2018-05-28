8101c3844875f85a6862c79af825fa8648af0c81
ivanderwilliam <ivanderwilliam@gmail.com>
2018-05-29 01:27:25 +0700

    Update about.php

--- about.php
+++ about.php
@@ -7,11 +7,17 @@
    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.css">
     <link rel="stylesheet" href="style/card.css">
     <link rel="stylesheet" href="/css/master.css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.js">
+          <link href="style/bootstrap.min.css" rel="stylesheet" />
+    <link href="style/material-kit.css" rel="stylesheet"/>
+ <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.1.3/semantic.min.js"></script>
     <link rel="stylesheet" type="text/css" href="Semantic-UI-CSS-Master/semantic.min.css">
     <script src="Semantic-UI-CSS-Master/semantic.min.js"></script>
     <script src="https://code.jquery.com/jquery-2.1.4.js"></script>
+   
+
+
     <script>
     $(document).ready(function(){
         $('.login').click(function(){
@@ -23,9 +29,7 @@
     <title>Welcome</title>
   </head>
   <body>
-<div class="backgroundcolor">
-  <!--Header-->
-  <div class="ui inverted vertical masthead center aligned segment">
+<div class="ui inverted vertical masthead center aligned segment">
           <div class="ui center aligned container">
               <div class="ui large secondary inverted centered pointing compact menu">
                   <a class="toc item">
@@ -42,84 +46,87 @@
           </div>
 
       </div>
-<div class="ui modal" id="coba">
-      <div class="ui centered header" style="font-size:30px; color:white;">
-        Log In To Foodyshark
-      </div>
-     <div class="page-login">
-    <div class="ui centered grid container">
-      <div class="nine wide column">
-        <br><br>
-        <div class="ui fluid card">
-          <div class="content">
-          <form class="ui form" method="POST">
-            <div class="field">
-              <label>User</label>
-              <input type="text" name="user" placeholder="User">
+		<div class="container">
+            
+            <div class="about-team team-1">
+    			<div class="row">
+    				<div class="col-md-8 col-md-offset-2 text-center">
+    					<h2 class="title">We are Food Lovers</h2>
+    					<h5 class="description">We are a group of Food Lovers in Teknik Informatika ITS that decided to make a website for fellow food lovers to enjoy the things that they enjoy the most.</h5>
+    				</div>
+    			</div>
+
+    			<div class="row">
+    				<div class="col-md-4">
+    					<div class="card card-profile card-plain">
+    						<div class="card-avatar">
+    							<a href="#pablo">
+    								<img class="img" src="imgsource/chef_juna.jpg" />
+    							</a>
+    						</div>
+
+    						<div class="card-content">
+    							<h4 class="card-title">Rifqi</h4>
+    							<h6 class="category text-muted">CEO</h6>
+
+    							<p class="card-description">
+    								"It does not matter how slowly you go, so long as you do not stop."
+                                    <br>
+                                    -not by me
+    							</p>
+    							
+    						</div>
+    					</div>
+    				</div>
+
+    				<div class="col-md-4">
+    					<div class="card card-profile card-plain">
+    						<div class="card-avatar">
+    							<a href="#pablo">
+    								<img class="img" src="imgsource/Gordon-Ramsay.jpg" />
+    							</a>
+    						</div>
+
+    						<div class="card-content">
+    							<h4 class="card-title">Aang</h4>
+    							<h6 class="category text-muted">Front End Developer</h6>
+
+    							<p class="card-description">
+    								Live as if you were to die tomorrow. Learn as if you were to live forever.
+    							</p>
+    							
+    						</div>
+    					</div>
+    				</div>
+
+    				<div class="col-md-4">
+    					<div class="card card-profile card-plain">
+    						<div class="card-avatar">
+    							<a href="#pablo">
+    								<img class="img" src="imgsource/rav-cropped.jpg" />
+    							</a>
+    						</div>
+
+    						<div class="card-content">
+    							<h4 class="card-title">Ivander William</h4>
+    							<h6 class="category text-muted">Front End Developer</h6>
+
+    							<p class="card-description">
+    								I have not failed. I've just found 10,000 ways that won't work
+    							</p>
+    							
+    						</div>
+    					</div>
+    				</div>
+
+    				
+    			</div>
+
             </div>
-            <div class="field">
-              <label>Password</label>
-              <input type="password" name="pass" placeholder="Password">
-            </div>
-              <div class="ui grid">
-                  <div class="four column row">
-                      <div class="centered column">
-                      <button class="ui primary button" type="submit">
-                              <a href="dashboard.html">Log In</a>
-                      </button>
-                      </div>
-                  </div>
-              </div>
-          </form>
-          </div>
+            
+            
+            
         </div>
-      </div>
     </div>
-  </div>
-</div>
-<div>
-
-<div class="maincontainer" style="  top: 50%;
-  left: 20%;">
-
-      <div class="thecard">
-
-        <div class="thefront"><img src="imgsource/Gordon-Ramsay.jpg"></div>
-
-        <div class="theback"><h1>Ivander William</h1><p>Front End Developer of This Web</p>
-        </div>
-
-      </div>
-    </div>
-<div class="maincontainer" style="  top: 50%;
-  left: 50%;">
-
-      <div class="thecard">
-
-        <div class="thefront"><img src="imgsource/rav-cropped.jpg"></div>
-
-        <div class="theback"><h1>Aang</h1><p>Front End Developer of This Web</p>
-      </div>
-
-      </div>
-    </div>
-<div class="maincontainer" style="  top: 50%;
-  left: 80%;">
-
-      <div class="thecard">
-
-        <div class="thefront"><img src="imgsource/chef_juna.jpg"></div>
-
-        <div class="theback"><h1>Rifqi</h1><p>Back End Developer of This Web</p>
-        <button>Submit</button></div>
-
-      </div>
-    </div>
-      </div>  
-      </div>
-</body>
-  <script>
-    $('.login.modal')
-    .modal('attach events', '.login.button','show');
-  </script>
+    </body>
 </html>
