e0e43e689a5179803660702b81f442f0029f565e
ivanderwilliam <ivanderwilliam@gmail.com>
2018-05-29 03:22:26 +0700

    Create bayar.php

---
+++ bayar.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="Logo Website.png">
+   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.css">
+    <link rel="stylesheet" href="style/card.css">
+    <link rel="stylesheet" href="/css/master.css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.js">
+          <link href="style/bootstrap.min.css" rel="stylesheet" />
+    <link href="style/material-kit.css" rel="stylesheet"/>
+ <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.1.3/semantic.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="Semantic-UI-CSS-Master/semantic.min.css">
+    <script src="Semantic-UI-CSS-Master/semantic.min.js"></script>
+    <script src="https://code.jquery.com/jquery-2.1.4.js"></script>
+    </head>
+
+<body>
+<div class="ui inverted vertical masthead center aligned segment">
+          <div class="ui center aligned container">
+              <div class="ui large secondary inverted centered pointing compact menu">
+                  <a class="toc item">
+                      <i class="sidebar icon"></i>
+                  </a>
+                  <a class="active item">Home</a>
+                  <a class="item" href="#">Order</a>
+                  <a class="item login" id="testing">Login</a>
+                  <a class="item" href="signup.html">Sign Up</a>
+              </div>
+          </div>
+
+          <div class="ui text container">
+          </div>
+
+      </div>    
+    
+<div class="pricing-1 section-image">
+
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3 text-center">
+					<h2 class="title">Fatten your wallet now</h2>
+					<h5 class="description">Fatten your wallet before you fatten your stomach, deposit more to get more bonus from FoodyShark</h5>
+					<div class="section-space"></div>
+				</div>
+			</div>
+
+			<div class="row">
+
+				<div class="col-md-3">
+					<div class="card card-pricing card-plain">
+						<div class="card-content">
+							<h6 class="category">Bronze</h6>
+							<div class="icon">
+								<i class="material-icons">sentiment_very_dissatisfied</i>
+							</div>
+							<h3 class="card-title">Rp.25.000</h3>
+							<p class="card-description">
+                                Breakfast/Lunch/Dinner for 1 person
+                            </p>
+							<a href="#pablo" class="btn btn-white btn-round">Choose Plan</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-3">
+					<div class="card card-pricing card-plain">
+						<div class="card-content">
+							<h6 class="category">Silver</h6>
+							<div class="icon icon-rose">
+								<i class="material-icons">sentiment_dissatisfied</i>
+							</div>
+							<h3 class="card-title">Rp.75.000</h3>
+							<p class="card-description">
+                                Packet for 2 days of full course food
+                            </p>
+							<a href="#pablo" class="btn btn-white btn-round">Choose Plan</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-3">
+					<div class="card card-pricing card-plain">
+						<div class="card-content">
+							<h6 class="category">Gold</h6>
+							<div class="icon">
+								<i class="material-icons">mood</i>
+							</div>
+							<h3 class="card-title">Rp.125.000</h3>
+							<p class="card-description">
+                                I promise this won't dissapoint you, Food is Love , Food is Life
+                                <br>
+                                4 days of Full course Food
+                            </p>
+							<a href="#pablo" class="btn btn-white btn-round">Choose Plan</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-3">
+					<div class="card card-pricing card-plain">
+						<div class="card-content">
+							<h6 class="category">Platinum</h6>
+							<div class="icon">
+								<i class="material-icons">restaurant</i>
+							</div>
+							<h3 class="card-title">Rp.200.000</h3>
+							<p class="card-description">
+                                The best option to fill your wallet and tummy
+                                <br>
+                                1 week full of healthy food
+                            </p>
+							<a href="#pablo" class="btn btn-white btn-round">Choose Plan</a>
+						</div>
+					</div>
+				</div>
+
+			</div>
+
+		</div>
+	</div>
+    </body>
+</html>
